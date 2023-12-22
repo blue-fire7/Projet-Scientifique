1f6cf72351c715ebb3ff7faee723b9bc5f9e6cc5
Ymeerj <jeremy.dplc@gmail.com>
2023-12-22 09:44:14 +0100

    add initScript and change python simulator

--- Dockers/PostgreSQL/initTable.sql
+++ Dockers/PostgreSQL/initTable.sql
@@ -1,60 +1,60 @@
-Table sensor_fire {
-  id integer [primary key]
-  longitude integer
-  latitude integer
-  start_at datetime
-  edit_at datetime
-  level integer
-}
-
-Table fire_station {
-  id integer [primary key]
-  longitude integer
-  latitude integer
-
-}
-
-Table fire_truck {
-  id integer [primary key]
-  fire_station_id integer [ref: > fire_station.id]
-  fire_truck_type_id integer [ref: > fire_truck_type.id]
-  longitude integer
-  latitude integer
-}
-
-Table fire_fighter {
-  id integer [primary key]
-  firstname String
-  surname String
-  energy integer
-  in_mission bool
-  fire_station_id integer [ref: > fire_station.id]
-  team_id integer [ref: > team.id]
-
-}
-Table fire_truck_type {
-  id integer [primary key]
-  type String
-}
-
-Table fire {
-  id integer [primary key]
-  is_finished bool
-
-}
-Table sensor_event {
-  sensor_id integer [primary key, ref: > sensor_fire.id]
-  fire_id integer [primary key, ref: > fire.id]
-}
-Table team {
-  id integer [primary key]
-  name String
-}
-Table intervention {
-  id integer [primary key]
-  team_id integer [primary key, ref: > team.id]
-  fire_truck_id integer [primary key, ref: > fire_truck.id]
-  fire_id integer [primary key, ref: > fire.id]
-  taken_at datetime
-  arrival_at datetime
-}
+drop table if exists sensor;
+drop table if exists fire;
+drop table if exists sensor_event;
+drop table if exists team;
+drop table if exists fire_station;
+drop table if exists fire_truck;
+drop table if exists fire_truck_type;
+create table sensor(
+  id serial primary key,
+  longitude decimal(9, 6) not null,
+  latitude decimal(9, 6) not null
+);
+create table fire (
+  id serial primary key,
+  started_at timestamp not null,
+  ended_at timestamp
+);
+create table sensor_event(
+  sensor_id bigint references sensor(id),
+  fire_id bigint references fire(id),
+  update_at timestamp,
+  level integer not null,
+  constraint sensor_event_pkey primary key(sensor_id, fire_id, update_at)
+);
+create table fire_station(
+  id serial primary key,
+  longitude decimal(9, 6) not null,
+  latitude decimal(9, 6) not null
+);
+create table team(
+  id serial primary key,
+  name varchar(50) not null
+);
+create table fire_truck_type(
+  id serial primary key,
+  type varchar(50) not null,
+  speed integer not null
+);
+create table fire_truck(
+  id serial primary key,
+  fire_station_id bigint references fire_station(id) not null,
+  fire_truck_type_id bigint references fire_truck_type(id) not null,
+  longitude decimal(9, 6) not null,
+  latitude decimal(9, 6) not null
+);
+create table intervention(
+  team_id bigint references team(id) not null,
+  fire_truck_id bigint references fire_truck(id) not null,
+  fire_id bigint references fire(id) not null,
+  path path,
+  constraint intervention_pkey primary key(team_id, fire_truck_id, fire_id)
+);
+create table fire_fighter(
+  id serial primary key,
+  firstname varchar(50) not null,
+  surname varchar(50) not null,
+  energy integer default 10,
+  fire_station_id bigint references fire_station(id) not null,
+  team_id bigint references team(id) not null
+);
--- PythonReceiver/receiver.py
+++ PythonReceiver/receiver.py
@@ -15,25 +15,25 @@ def simulateFireSensor(mqttClient: mqtt.Client):
   Sa durée est de 20 secondes, passant a 2 avant et incrémentant un autre capteur avant detre réduit en 4 secondes
   """
   print('début feu')
-  mqttClient.publish('fire/sensor','fire_level,id=12 level=0')
+  mqttClient.publish('fire/sensor','fire_level,id=8 level=0')
 
   for i in range(10):
-    mqttClient.publish('fire/sensor','fire_level,id=12 level=1')
+    mqttClient.publish('fire/sensor','fire_level,id=8 level=1')
     time.sleep(1)
 
   print('ca va augmenter')
-  for i in range(5):
-    mqttClient.publish('fire/sensor','fire_level,id=12 level=2')
-    mqttClient.publish('fire/sensor','fire_level,id=23 level=1')
+  for i in range(10):
+    mqttClient.publish('fire/sensor','fire_level,id=8 level=2')
+    mqttClient.publish('fire/sensor','fire_level,id=9 level=1')
     time.sleep(1)
 
   print('pompier arrive')
-  for i in range(3):
-    mqttClient.publish('fire/sensor','fire_level,id=12 level=1')
-    mqttClient.publish('fire/sensor','fire_level,id=23 level=0')
+  for i in range(10):
+    mqttClient.publish('fire/sensor','fire_level,id=8 level=1')
+    mqttClient.publish('fire/sensor','fire_level,id=9 level=0')
     time.sleep(1)  
 
-  mqttClient.publish('fire/sensor','fire_level,id=12 level=0')
+  mqttClient.publish('fire/sensor','fire_level,id=8 level=0')
   
   return
 
